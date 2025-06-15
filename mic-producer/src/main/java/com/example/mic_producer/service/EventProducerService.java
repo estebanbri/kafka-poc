d9668a808ff8793dd2a3e9a83575af8e6b0b7cef
Nacho <@hotmail.com>
2025-06-16 00:09:41 +0200

    JsonSerializer

--- mic-producer/src/main/java/com/example/mic_producer/service/EventProducerService.java
+++ mic-producer/src/main/java/com/example/mic_producer/service/EventProducerService.java
@@ -4,18 +4,30 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class EventProducerService {
+
+    private static final String SUCCESS_LOG_MESSAGE = "Mensaje enviado: {}, partition: {}, offset: {}";
+    private static final String ERROR_LOG_MESSAGE = "Mensaje no enviado: {}, motivo: {}";
     @Value("${topic.name}")
     private String topicName;
 
     private final KafkaTemplate<String, String> kafkaTemplate;
     public void sendTopic(String message) {
-       kafkaTemplate.send(topicName, message);
-        log.info("Mensaje enviado: " + message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info(SUCCESS_LOG_MESSAGE, message, result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
+            } else {
+                log.error(ERROR_LOG_MESSAGE, message, ex.getMessage());
+            }
+        });
     }
 }
